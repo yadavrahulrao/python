6231613a8b65d13601db1e08794ca9fd78452087
RAHUL <rahul507538@gmail.com>
2025-03-27 21:35:11 +0530

    Add files via upload

---
+++ kpositive.py
@@ -0,0 +1,17 @@
+def positive(arr,k):
+  missing = 0
+  current = 1
+  i = 0
+  while missing < k:
+    if i < len(arr) and arr[i] == current :
+      i += 1
+    else :
+      missing += 1
+      if missing == k:
+        return current
+    current +=1
+  return -1
+arr = [1,2,4,5,7,9]
+k = 1
+c = positive(arr,k)
+print(f"{k} missing element in array is :{c}")
