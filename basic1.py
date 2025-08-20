de2c2238fe16505c646a8f567076e7a646e9da93
yadavrahulrao <rahul507538@gmail.com>
2025-08-20 22:41:20 +0530

    b1

---
+++ basic1.py
@@ -0,0 +1,8 @@
+a = """Twinkle, twinkle, little star, 
+\t How I wonder what you are!
+\t \t Up above the world so high, 
+\t \t Like a diamond in the sky.
+Twinkle, twinkle, little star,
+\t How I wonder what you are"""
+
+print(a)
