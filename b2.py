89f5e9ceda7ec670ea9091b557a8683d24502a30
yadavrahulrao <rahul507538@gmail.com>
2025-10-12 22:15:03 +0530

    w3

--- b2.py
+++ b2.py
@@ -1,3 +1,93 @@
-import sys
+# # import sys
 
-print(sys.version)
+# # print(sys.version)
+
+# # a = 12
+# # print(a)
+
+# import math 
+
+# print(math.e)
+
+# n = int (input("n = "))
+# if n < 10 :
+#   print("true")
+
+# else :
+#   print("false")
+
+# x = "John"
+# y = 'John'
+
+# if x == y :
+#   print("true")
+# else :
+#   print("false")
+
+# print(type(x))
+
+# print('hello','world')
+
+# a = 5
+# b = 'world'
+# print(a+b)
+
+# x = "rahul"
+# def fun():
+#   global x
+#   x = "rohit"
+# fun()
+# print("my name is"+x)
+
+# data types
+# variables are used to store different data types to perform different operations.abs
+
+# print(type('rahul'))
+
+# print(type(5))
+# print(type(1.4))
+# print(type(2j))
+# print(type(["apple","banana"]))
+# print(type(("rahul","yadav")))
+# print(type(range(6)))
+# print(type({"name":"rahul"}))
+
+# print(type(True))
+# print(type(None))
+# print(type(b"rahul"))
+
+#random number - python take a random number to show on display by the use of random module in th range
+
+# import random
+
+# print(random.randrange(0,10))
+
+# a complex number can not be converted into int , float .
+
+# for multi string we use triplle quotes """    """
+
+# a = "hello"
+# print(a[1])
+
+# for x in "rahul":
+#   print(x)
+
+# a = "rahul"
+# print(len(a))
+
+
+# gave bools datatype as answer
+# b = "here is the string for test"
+# print("is" in b)
+
+# if "the" in b:
+#   print("rahul")
+
+# print("for" not in b)
+# def test(data):
+#   data["a"] = 1
+# data = {"a":0}
+# test(data)
+# print(data)
+
+
---
+++ b3.py
@@ -0,0 +1,61 @@
+# # slicing
+
+# x = "welcome"
+# print(x[3:5])
+
+# txt = "hello world"
+# x = txt[2:5]
+# print(x)
+
+# x = "welcome"
+# print(x[3:])
+
+# a = "hello rahul"
+# print(a.upper())
+
+# b  = "DEEP"
+# print(b.lower())
+
+
+#remove whitespace 
+# a = "    rahul "
+# print(a.strip())
+
+# print(a.replace("r" , "D"))
+
+# print('welcome'.upper())
+
+# combine two string using + operator
+# a = "hello"
+# b = "rahul"
+# c = a+b
+# print(c)
+# print(a + " " + b)
+
+# we can combine string and number using f- string (format string)
+
+# print(f"hellothere is a format string {30}")
+
+#placeholder can contain modifier , operator , function
+#modifier is used to get the number for the decimal places. uses :.2f
+
+# print(f"heellllo{23:.2f}")
+
+# print(f"hellokjdhjg{20*2}")
+
+# escape character \ 
+
+# print("hello \"hello\"")
+#single quote
+# print('hi \'hi\'')
+# print("hi\\")  #backslash
+# print("hello\n rahul") # new line
+
+# print("hello \t rahul") # tab sapace
+
+# print("hello\r rahul \r go") # carriage return
+
+# print("\110\220\210\340\120")  # octal values
+
+
+print("\x45")
