d35315e379590aacea69670c56580be791acdb2f
yadavrahulrao <rahul507538@gmail.com>
2025-10-19 20:51:27 +0530

    b5

--- b5.py
+++ b5.py
@@ -42,3 +42,75 @@ a = [1,2,4]
 
 # [print(x) for x in a]
 
+# list comprehension is for crating a ne list from the exixting list
+
+a = ["abc" ,"bcd", "akjhdsf","ksdgf"]
+
+# b = []
+# for i in a :
+#   if "a" in i:
+#     b.append(i)
+# print(b)
+
+
+# b = [x for x in a if "a" in x]
+# print(b)
+
+
+# a = ["apple" ,"banana" , "cherry"]
+
+# a.sort()
+# n = ["apple" for x in a ]
+# print(a)
+
+# using sort()  is a case insensitive sort .
+
+# x = [100,20,30,10,40,80]
+# x.sort()
+# x.sort(reverse=True)
+
+# x.reverse()
+
+# print(x)
+
+
+# def fun(n):
+#   return abs(n - 50)
+
+# a = [100,30,20,50,40,10]
+# a.sort(key=fun)
+# print(a)
+
+# we can use copy(), list. , or slicing for copy of one list to another
+
+# a = [20 , 10 , 33 , 44]
+# x = a.copy()
+# print(x)
+
+# x = list(a)
+# print(x)
+
+# x = a[:]
+# print(x)
+
+
+# we can join two lists using +operator , using append and extend.
+
+# l1 = [23,34,45,56]
+# l2 = [23,324,345,456]
+# print(l1+l2)
+
+
+# l1 = ["rao"]
+# l2 = [34]
+# for x in l2:
+#   l1.append(x)
+# print(l1)
+
+
+# l1 = [324]
+# l2 = ["rahul"]
+# l1.extend(l2)
+# print(l1)
+
+
