30a1f89fe45ef8c6578724b1efd1ac61a92194c9
yadavrahulrao <rahul507538@gmail.com>
2025-10-23 20:12:32 +0530

    b7

---
+++ b7.py
@@ -0,0 +1,14 @@
+#sets
+# s = "cat"
+# print("".join(reversed(s)))
+
+
+# num = 25
+# if num % 5 == 0:
+#   if num % 2 == 0 :
+#     print("java")
+#   else :
+#     print("python")
+# else :
+#   print("c++")
+
