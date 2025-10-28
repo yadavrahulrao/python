943d4577cfe19668520f307a623c8dc362fd79be
yadavrahulrao <rahul507538@gmail.com>
2025-10-28 21:20:31 +0530

    b7

--- b7.py
+++ b7.py
@@ -12,3 +12,27 @@
 # else :
 #   print("c++")
 
+#sets are unordered , unchangeable , unindexed
+#sets are unordered so this means in what order they could appear
+# sets are unchangeable but we can add or remove items.
+
+# duplicates are not aloowed in sets
+
+# a = {"apple", "apple"}
+# print(a)
+
+# in sets true and 1 are considered as same and duplicates
+
+# a = {True,1}
+# print(a)
+
+#in sets flase and 0  are considered as same and duplicates
+
+# a = {False,0}
+# print(a)
+
+# we can use set() constructor to construct a set using double brackets 
+
+# a = set(("apple"))
+# print(a)
+
