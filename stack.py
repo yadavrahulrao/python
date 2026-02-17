b6b5d4d9b67407f63e4a68267842258c31ec324a
yadavrahulrao <rahul507538@gmail.com>
2026-02-17 22:41:36 +0530

    stack

--- stack.py
+++ stack.py
@@ -21,30 +21,127 @@
 
 #stack using array and class 
 
-class Stack :
+# class Stack :
+#   def __init__(self):
+#     self.stack = []
+
+#   def push(self,element):
+#     self.stack.append(element)
+
+#   def top(self):
+#     if self.isempty():
+#       return "stack is empty"
+#     return self.stack[-1]
+
+#   def delete(self):
+#     if self.isempty():
+#       return "stack is empty"
+#     return self.stack.pop()
+  
+#   def isempty(self):
+#     return len(self.stack) == 0
+
+#   def size(self):
+#     return len(self.stack)
+
+# m = Stack()
+# m.push('a')
+# m.push('b')
+# m.push('c')
+
+# print(m.stack)
+
+# m.delete()
+# print(m.stack)
+
+  
+#  stack using linked list 
+
+
+# class node:
+#   def __init__(self,value):
+#     self.value = value
+#     self.next = None
+
+# class Stack:
+#   def __init__(self):
+#     self.head = None
+#     self.size = 0
+
+#   def push(self,value):
+#     new = node(value)
+#     if self.head :
+#       new.next = self.head
+#     self.head = new
+#     self.size += 1
+
+#   def isempty(self):
+#     return self.size == 0
+
+#   def delete(self):
+#     if self.isempty:
+#       print("stack is empty")
+
+#     poped = self.head
+#     self.head =  self.head.next 
+#     self.size -= 1
+#     return poped.value
+
+#   def top(self):
+#     if self.isempty:
+#       print("stack is empty")
+#     return self.head.value
+
+#   def stack_size(self):
+#     return self.size
+
+#   def traverse(self):
+#     curr = self.head
+#     while curr :
+#       print(curr.value,end="-->")
+#       curr = curr.next 
+#     print()
+
+# m = Stack()
+
+# m.push('a')
+# m.push('b')
+# m.push('c')
+# m.push(2)
+
+# print(m.traverse())
+
+# print(m.stack_size())
+
+
+
+
+#question 
+
+class Stack:
   def __init__(self):
-    self.stack = []
+    
+    self.stack = ["a","b","c"]
 
   def push(self,element):
     self.stack.append(element)
-
-  def top(self):
-    if self.isempty():
-      return "stack is empty"
-    return self.stack[-1]
+  
+  # def isempty(self):
+  #   return len(self.stack) == 0
 
   def delete(self):
-    if self.isempty():
-      return "stack is empty"
-    return self.stack.pop()
-  
-  def isempty(self):
-    return len(self.stack) == 0
+    
+    return self.stack.pop(0)
 
-  def size(self):
-    return len(self.stack)
+  def peek(self):
+    return self.stack[-1]
+
 
 m = Stack()
-m.stack.push('a')
+m.push('d')
+print(m.stack)
 
-  
+m.delete()
+print(m.stack)
+
+print(m.peek())
