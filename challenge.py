1c6e2918e24df01529ab334c252aa7a87e43cf53
yadavrahulrao <rahul507538@gmail.com>
2026-03-10 13:59:12 +0530

    py

--- challenge.py
+++ challenge.py
@@ -1,6 +1,68 @@
-class A:
-  data = []
-a = A()
-b = A()
-a.data.append(5)
-print(b.data)
+# class A:
+#   data = []
+# a = A()
+# b = A()
+# a.data.append(5)
+# print(b.data)
+
+# for i in range(1,10,3):
+#   print(i)
+#   if(i==4):
+#     break
+
+
+# for i in range(3):
+#   print(i)
+
+# else :
+#   print("code loop completed without break")
+
+
+# 5 + 7
+# _ + 3
+
+
+# name = ["rahul","rohit"]
+# age = [20,22]
+# for n,a in zip(name,age):
+#   print(f"name is {n},age is {a}")
+
+
+# eq = '130 + 132 = 262'
+# for i in eq:
+#   if i not in '11':
+#     print(i)
+
+
+# class shape :
+#   def area(self):
+#     return 0
+# class circle(shape):
+#   def __init__(self,radius):
+#     self.radius = radius
+#   def area(self):
+#     return 3.14 * self.radius * self.radius
+
+# c = circle(2)
+# r = c.area()
+# print(r)
+
+
+# n = [1,2,3,4,5]
+# t = 0
+# for i in n :
+#   t += i
+# print(t)
+
+
+# t= (1,2,3,4)
+# t= (1,2,3,4)
+# r = t +(5,)
+# print(r)
+# r = t +(5,)
+# print(r)
+
+def power(x,n):
+  return x**n
+r = power(2,3)
+print(r)
